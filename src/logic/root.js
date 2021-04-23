9d0cfcdcb668a85c2381a71980067b80908835d9
robertrinearson <rrinearsonjr@gmail.com>
2021-04-23 13:03:43 -0500

    stuff

--- src/logic/root.js
+++ src/logic/root.js
@@ -10,14 +10,14 @@ import { smooth } from 'modules/scroll';
 // components
 import header from 'headers/1/_';
 import nav from 'navigation/1/_';
-import footer from 'footers/1/_';
+// import footer from 'footers/1/_';
 
 // EXECUTION
 document.addEventListener('DOMContentLoaded', () => {
 
-    nav();
+    // nav();
     smooth();
     header();
-    footer();
+    // footer();
 
 });
